918d39a2590f8e9c480ca603c333f4e0b65685fb
e-man <tenhitcombo@gmail.com>
2022-07-07 00:19:45 -0400

    time to get all object oriented

--- AlgoMonsterCPP/AlgoMonsterCPP.cpp
+++ AlgoMonsterCPP/AlgoMonsterCPP.cpp
@@ -1,10 +1,6 @@
-#include "solo.h"
+//#include "solo.h"
 
 int main()
 {   
-	int megabytes;
-	cin >> megabytes;
-	cout << "Before promotion: " << megabytes << endl;
-	promotion(&megabytes);
-	cout << "After promotion: " << megabytes << endl;
+	
 }
